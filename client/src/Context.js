5e8e1593f56cf72defcdd58e622dcd44726f63df
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-12 01:36:10 +0200

    Add Authenticated User State

--- client/src/Context.js
+++ client/src/Context.js
@@ -5,13 +5,19 @@ const Context = React.createContext();
 
 export class Provider extends Component {
 
+  state = {
+    authenticatedUser: null
+  };
+
   constructor() {
     super();
     this.data = new Data();
   }
 
   render() {
+    const { authenticatedUser } = this.state;
     const value = {
+      authenticatedUser,
       data: this.data,
       actions: { // Add the 'actions' property and object
         signIn: this.signIn
