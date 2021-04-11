51305751a9a0ffc521ac8c628229bfb422a1e31a
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-12 01:27:18 +0200

    Create the Submit Function

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -20,27 +20,27 @@ export default class UserSignIn extends Component {
       <div className="bounds">
         <div className="grid-33 centered signin">
           <h1>Sign In</h1>
-          <Form 
+          <Form
             cancel={this.cancel}
             errors={errors}
             submit={this.submit}
             submitButtonText="Sign In"
             elements={() => (
               <React.Fragment>
-                <input 
-                  id="username" 
-                  name="username" 
+                <input
+                  id="username"
+                  name="username"
                   type="text"
-                  value={username} 
-                  onChange={this.change} 
+                  value={username}
+                  onChange={this.change}
                   placeholder="User Name" />
-                <input 
-                  id="password" 
+                <input
+                  id="password"
                   name="password"
                   type="password"
-                  value={password} 
-                  onChange={this.change} 
-                  placeholder="Password" />                
+                  value={password}
+                  onChange={this.change}
+                  placeholder="Password" />
               </React.Fragment>
             )} />
           <p>
@@ -63,7 +63,19 @@ export default class UserSignIn extends Component {
   }
 
   submit = () => {
-
+    const { context } = this.props;
+    const { username, password } = this.state;
+    context.actions.signIn(username, password)
+        .then( user => {
+          if (user === null) {
+            this.setState(() => {
+              return { errors: [ 'Sign-in was unsuccessful' ] };
+            });
+          } else {
+            this.props.history.push('/authenticated');
+            console.log(`SUCCESS! ${username} is now signed in!`);
+          }
+        })
   }
 
   cancel = () => {
