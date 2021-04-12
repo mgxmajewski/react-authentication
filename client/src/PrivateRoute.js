d0216291fcd3a8df66db05281fabc3979016069f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-12 02:04:29 +0200

    Redirect the User (if not authenticate)

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -4,13 +4,18 @@ import { Consumer } from './Context';
 
 export default ({ component: Component, ...rest }) => {
   return (
-    <Consumer>
-      { context => (
-        <Route
-          {...rest}
-          render={}
-        />
-      )}
-    </Consumer>
+      <Consumer>
+        { context => (
+            <Route
+                {...rest}
+                render={props => context.authenticatedUser ? (
+                        <Component {...props} />
+                    ) : (
+                        <Redirect to='/signin' />
+                    )
+                }
+            />
+        )}
+      </Consumer>
   );
 };
