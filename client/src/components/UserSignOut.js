71e258b332db14c1a6b7db66f2b99048ac8f1d62
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-12 02:26:55 +0200

    Redirect the User to the Root Route on Sign-out

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+export default ({context}) => {
+  // component calls signOut and updates state after render
+  useEffect(() =>  context.actions.signOut());
 
-export default () => {
   return (
-    <Redirect to="/" />
+      <Redirect to="/" />
   );
 }
